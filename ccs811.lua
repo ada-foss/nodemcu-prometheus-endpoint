07bd881c7d198099345457a1ae6e85858fa3ea88
Berin Smaldon <noodels555@gmail.com>
2019-05-20 00:30:38 +0100

    working commit for ccs811

--- ccs811.lua
+++ ccs811.lua
@@ -1,21 +1,98 @@
 local i2c = require('i2c')
+local bit = require('bit')
+local tmr = require('tmr')
 local ccs811 = { }
 
-ccs811.read_co2 = function()
+local cache = { }
+local cache_expiry = 10
+local cache_age = -1 - cache_expiry
+
+local function i2c_read_reg(addr, reg, len)
     i2c.start(0)
-    i2c.address(0, 91, i2c.TRANSMITTER)
-    i2c.write(0, 0x02)
+    i2c.address(0, addr, i2c.TRANSMITTER)
+    i2c.write(0, reg)
     i2c.stop(0)
+
     i2c.start(0)
-    i2c.address(0, 91, i2c.RECEIVER)
-    local r = i2c.read(0, 2)
+    i2c.address(0, addr, i2c.RECEIVER)
+    local r = i2c.read(0, len)
     i2c.stop(0)
-    return r:byte(0,1)*256 + r:byte(1,2)
+    return r
 end
 
-i2c.start(0)
-i2c.address(0, 91, i2c.TRANSMITTER)
-i2c.write(0, 0x01, 0x10)
-i2c.stop(0)
+local function update_cache()
+    if tmr.time() - cache_age > cache_expiry then
+        local r = i2c_read_reg(90, 0x02, 6)
+
+        cache.co2 = r:byte(0,1)*256 + r:byte(1,2)
+        cache.tvoc = r:byte(2,3)*256 + r:byte(3,4)
+        cache.status = r:byte(5,6)
+        cache.error_id = r:byte(4,5)
+
+        cache_age = tmr.time()
+    end
+end
+
+ccs811.read_co2 = function()
+    update_cache()
+    return cache.co2
+end
+
+ccs811.read_tvoc = function()
+    update_cache()
+    return cache.tvoc
+end
+
+ccs811.read_status = function()
+    update_cache()
+    return cache.status
+end
+
+ccs811.read_error_id = function()
+    update_cache()
+    return cache.error_id
+end
+
+ccs811.read_true_status = function()
+    return i2c_read_reg(90, 0x00, 1):byte(0,1)
+end
+
+ccs811.read_meas_mode = function()
+    return i2c_read_reg(90, 0x01, 1):byte(0,1)
+end
+
+ccs811.read_error_code = function()
+    return i2c_read_reg(90, 0xE0, 1):byte(0,1)
+end
+
+print("initializing ccs811...")
+
+local status = i2c_read_reg(90, 0x00, 1):byte(0,1)
+print(string.format("     status register: 0x%.2x", status))
+
+local app_valid = bit.band(status, 0x10)
+
+if not app_valid then
+    print('ERROR: CCS811 firmware not valid, not starting CCS811')
+else
+    print("starting ccs811...")
+    -- application start register
+    i2c.start(0)
+    i2c.address(0, 90, i2c.TRANSMITTER)
+    i2c.write(0, 0xF4)
+    i2c.stop(0)
+
+    -- measurement mode register
+    i2c.start(0)
+    i2c.address(0, 90, i2c.TRANSMITTER)
+    i2c.write(0, 0x01, 0x20)
+    i2c.stop(0)
+
+    -- re-read status
+    status = i2c_read_reg(90, 0x00, 1):byte(0,1)
+end
+
+print(string.format("     status register: 0x%.2x", status))
+print(string.format("   error id register: 0x%.2x", i2c_read_reg(90, 0xE0, 1):byte(0,1)))
 
 return ccs811
--- metrics.lua
+++ metrics.lua
@@ -1,4 +1,6 @@
 
+print('loading all metrics..')
+
 do
     local i2c = require('i2c')
     -- sda, GPIO4, D2, pin 2
@@ -9,7 +11,7 @@ end
 --local adc_experiment = require('adc_experiment')
 local temperature = require('temperature')
 local mcp3008 = require('mcp3008')
-local bme280_binding = require('bme280_binding')
+--local bme280_binding = require('bme280_binding')
 local ccs811 = require('ccs811')
 local metrics = { }
 
@@ -39,10 +41,16 @@ for i=0,7 do
     metrics[('mcp3008_reading{chip="%s",pin="%s"}'):format(adc_pin, i)] = mcp3008.bind_to_adc(adc_pin, i)
 end
 
-metrics.bme280_temperature_celsius = bme280_binding.temperature
-metrics.bme280_pressure_hectopascals = bme280_binding.pressure
-metrics.bme280_humidity_percentage = bme280_binding.humidity
+--metrics.bme280_temperature_celsius = bme280_binding.temperature
+--metrics.bme280_pressure_hectopascals = bme280_binding.pressure
+--metrics.bme280_humidity_percentage = bme280_binding.humidity
 
 metrics.ccs811_co2_ppm = ccs811.read_co2
+metrics.ccs811_tvoc_ppb = ccs811.read_tvoc
+metrics.ccs811_status = ccs811.read_status
+--metrics.ccs811_error_id = ccs811.read_error_id
+--metrics.ccs811_true_status = ccs811.read_true_status
+--metrics.ccs811_measure_mode = ccs811.read_meas_mode
+--metrics.ccs811_error_code = ccs811.read_error_code
 
 return metrics
